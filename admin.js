37e566f1c0db832d048cacc23e8e45855bcd04d0
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-10-17 23:08:30 +0530

    Admin added

---
+++ admin.js
@@ -0,0 +1,21 @@
+const { Kafka } = require('./client');
+
+async function init() {
+    const admin = Kafka.admin();
+    console.log('admin connecting...');
+    admin.connect();
+    console.log('admin connected!!!');
+    console.log("Creating Topic [rider-updates]");
+    await admin.createTopics({
+        topics: [{
+            topic: 'rider-updates',
+            numPartitions: 2
+        }]
+    })
+    console.log("Topic Created Success [rider-updates]");
+
+    console.log("Disconnecting Admin..");
+    admin.disconnect();
+}
+
+init();
